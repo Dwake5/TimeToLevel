60237db04164ea11195c60cecb2c2196f63cd482
Danny Wakeling <45290034+Dwake5@users.noreply.github.com>
2019-12-10 23:15:31 +0000

    Update TimeToLevel.js

--- TimeToLevel.js
+++ TimeToLevel.js
@@ -1,13 +1,3 @@
-// This code tells you how long until a certain condition is met in a game.
-// Rivals are leveled up by masters, of which there are three, in no order.
-// The rival needs level*10+10 exp to level up. 
-// They get each (master lvl - rival lvl) exp per min if greater than zero or zero otherwise.
-// How long will it take for a rival to reach each masters level or a set target level?
-// Four inputs will return 1-3 outputs for how long it takes to get to each masters level.
-// Five inputs will return how long it takes to reach that target.
-
-// I posted it to the relevant forum and explained step by step how non-coders could use it. Because it has a highly desired output. 
-
 const timeToMax = (rival, m1, m2, m3, target=-1) => {
   [m1,m2,m3] = [m1,m2,m3].sort((a, b) => a > b ? 1 : -1)
 
